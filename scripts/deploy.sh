e62a9f75059ba048033bfa388c191ca3be3dde87
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-06-10 21:21:57 +0545

    updated docker to use the system variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,16 +15,12 @@ docker container rm -f go-app || true
 # remove dangling docker images
 docker image prune -f
 
-
-echo "HOST IS" $DB_HOST
-echo "SERVER IS" $DEV_HOST
-
 # run the docker container using go-app:latest image
 docker run -d --network kong-net -p 8080:8080 \
-  -e PORT=8080 \
-  -e DB_HOST=$DB_HOST \
-  -e DB_PORT=$DB_PORT \
-  -e DB_DATABASE=$DB_DATABASE \
-  -e DB_USERNAME=$DB_USERNAME \
-  -e DB_PASSWORD=$DB_PASSWORD \
+  -e "PORT=8080" \
+  -e "DB_HOST=postgres.cji8e0iywfpa.ap-south-1.rds.amazonaws.com" \
+  -e "DB_PORT=5432 "\
+  -e "DB_DATABASE=postgres" \
+  -e "DB_USERNAME=postgres" \
+  -e "DB_PASSWORD=postgres" \
   --name go-app go-app:latest
