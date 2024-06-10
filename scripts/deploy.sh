2d9f838324427b08eb72d329d7488b997c9c9954
Achyut Paudel <achyut.paudel@biworldwide.com>
2024-06-10 14:31:06 +0545

    added compression to the scp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,26 @@
 # build the image
 cd go-app
 
+pwd && ls
+
+# decompress the go-app tar file and remove the tar file
 tar -xzf go-app.tar.gz -C . && rm -f go-app.tar.gz
 
+# build the image using the file Dockerfile.deploy
 docker build -f ./build/package/Dockerfile.deploy -t go-app:latest .
+
+# remove the container
 docker container rm -f go-app || true
+
+# remove dangling docker images
 docker image prune -f
 
-docker run -d --network kong-net -p 8080:8080 -e  \
-  "PORT=8080" \
-  "DB_HOST=localhost" \
-  "DB_PORT=5433" \
-  "DB_DATABASE=postgres" \
-  "DB_USERNAME=postgres" \
-  "DB_PASSWORD=postgres" \
-   --name go-app go-app:latest
+# run the docker container using go-app:latest image
+docker run -d --network kong-net -p 8080:8080 \
+  -e PORT=8080 \
+  -e DB_HOST=localhost \
+  -e DB_PORT=5433 \
+  -e DB_DATABASE=postgres \
+  -e DB_USERNAME=postgres \
+  -e DB_PASSWORD=postgres \
+  --name go-app go-app:latest
